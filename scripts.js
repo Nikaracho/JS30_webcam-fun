c643ba18b7c1c901cf671d7bcfdaf53d56851a89
Nikaracho <nicolas.carrieres@gmail.com>
2019-04-04 20:33:39 -0300

    init project

--- scripts.js
+++ scripts.js
@@ -25,10 +25,10 @@ function paintToCanvas(){
 	return setInterval(() => {
 		ctx.drawImage(video, 0, 0, width, height);
 		let pixels = ctx.getImageData( 0, 0, width, height)
-		pixels = redEffect(pixels);
+		//pixels = redEffect(pixels);
 		//pixels = rgbSplit(pixels);
 		//ctx.globalAlpha = 0.01;
-		//pixels = greenScreen(pixels);
+		pixels = greenScreen(pixels);
 		ctx.putImageData(pixels, 0, 0);
 	}, 16);
 }
